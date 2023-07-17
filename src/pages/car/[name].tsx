79fc18160ae3e747a5a9c150fe3ad02fa98de081
Hdewani <1320203902@bitraipur.ac.in>
2023-07-18 01:20:12 +0530

    added increment

--- src/pages/car/[name].tsx
+++ src/pages/car/[name].tsx
@@ -1,9 +1,19 @@
-import React from 'react'
-import Router from 'next/router'
+import React, { useEffect, useState } from 'react'
+import {useRouter} from 'next/router'
 function cars() {
+    const router=useRouter()
+    const [CarName,SetCArName]=useState('')
+
+    useEffect(()=>{
+        const q=router.query.name as string
+        console.log(q)
+        if(q){
+            SetCArName(q)
+        }
+    }),[router]
   return (
     <div>
-      cars
+      cars <h1>{CarName}</h1>
     </div>
   )
 }
