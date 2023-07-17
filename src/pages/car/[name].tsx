9e1122fe6526054113f430d112d501a08434b8fa
Hdewani <1320203902@bitraipur.ac.in>
2023-07-18 00:21:01 +0530

    added basic files

---
+++ src/pages/car/[name].tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Router from 'next/router'
+function cars() {
+  return (
+    <div>
+      cars
+    </div>
+  )
+}
+
+export default cars
---
+++ src/pages/hitika/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function index() {
+  return (
+    <div>
+      Hitika's Home
+    </div>
+  )
+}
+
+export default index
